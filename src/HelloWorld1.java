304be7ebaf7cbcafacb4b70e1a3875876935281d
Abraham Bueno <abueno@localhost.localdomain>
2019-01-01 22:19:22 -0600

    more examples, too simple.

--- src/HelloWorld1.java
+++ src/HelloWorld1.java
@@ -3,5 +3,10 @@ public class HelloWorld1 {
     static final double PINUM = 3.141529;
     public static void main(String[] args) {
         System.out.println(PINUM);
+        int integerOne = 22;
+
+        int integerTwo = integerOne + 1;
+
+        System.out.println(integerTwo);
     }
 }
