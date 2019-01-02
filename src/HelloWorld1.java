6ce8b623f9ce226d3d2c758a68715603165acff4
Abraham Bueno <abueno@localhost.localdomain>
2019-01-01 22:26:38 -0600

    A bit more data types and more examples.

--- src/HelloWorld1.java
+++ src/HelloWorld1.java
@@ -6,7 +6,27 @@ public class HelloWorld1 {
         int integerOne = 22;
 
         int integerTwo = integerOne + 1;
-
         System.out.println(integerTwo);
+
+        byte bigByte = 127;
+        short bigShort = 32767;
+        int bigInt = 2100000000;
+        long bighLong = 922000000000000000L;
+        float bigFloat = 3.14F;
+        double bigDouble = 3.14283838338383;
+
+        System.out.println(Float.MAX_VALUE);
+        System.out.println(Double.MAX_VALUE);
+
+        boolean trueOrFalse = true;
+
+        char randomChar = 66;
+        char anotherChar = 'A';
+        char excapedChars = '\f'; // there are escape character
+        String randomString = "I'm a random string";
+        String anotherString = "Stuff";
+        String andAnotherString = randomString + ' ' + anotherString;
+
+        System.out.println(andAnotherString);
     }
 }
