512254c8d69fd0bc639856c03abdf3e6ed3212e0
Abraham Bueno <abueno@localhost.localdomain>
2019-01-03 17:17:19 -0600

    Simple if logic using the java operators.

--- src/JavaLesson3.java
+++ src/JavaLesson3.java
@@ -9,7 +9,20 @@ public class JavaLesson3 {
         != Not Equal to
         >= Greater than or Equal to
         <= Less than or equal to
-        
+
         * */
+
+        if (randomNumber < 25) {
+            System.out.println("The random number is less than 25");
+        } else if (randomNumber > 40) {
+            System.out.println("The random number is greater than " + randomNumber);
+
+        } else if (randomNumber == 18) {
+            System.out.println("The random number is equal to 18");
+        } else if (randomNumber != 40) {
+            System.out.println("The random number is not equal to 40");
+        } else {
+            System.out.println("Didn't match conditions " + randomNumber);
+        }
     }
 }
