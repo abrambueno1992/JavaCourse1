b3b7e1764a3e6cbec009c23bdf74ddaac6fdd717
Abraham Bueno <abueno@localhost.localdomain>
2019-01-03 17:12:36 -0600

    Comments with the 6 relational operators in java.

---
+++ src/JavaLesson3.java
@@ -0,0 +1,15 @@
+public class JavaLesson3 {
+    public static void main(String[] args) {
+        int randomNumber = (int) (Math.random() * 50);
+        /* Relational Operators:
+        6 in total
+        > Greater than
+        < Less than
+        == Equal to
+        != Not Equal to
+        >= Greater than or Equal to
+        <= Less than or equal to
+        
+        * */
+    }
+}
