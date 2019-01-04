c550d49296ca43ea4008e4a1827ac3729a469543
Abraham Bueno <abueno@localhost.localdomain>
2019-01-03 20:38:46 -0600

    Added two types of errors and most common exception errors.

---
+++ src/JavaLesson6.java
@@ -0,0 +1,18 @@
+// Types of errors
+// java.lang.RunTimeException needs to be taken care of
+// java.lang.Exception taken care of by Java
+
+// Common exceptions
+/*
+    ArithmeticException
+    ClassNotFoundException
+    IllegalArgumentException
+    IndexOutOfBoundsExceptions
+    InputMismatchException
+    IOException
+ */
+
+
+
+public class JavaLesson6 {
+}
