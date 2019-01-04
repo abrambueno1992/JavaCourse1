bab091230f8fa87101840a8f1d271abe9ae455b0
Abraham Bueno <abueno@localhost.localdomain>
2019-01-03 19:12:49 -0600

    Simple method that returns addition.

--- src/JavaLesson5.java
+++ src/JavaLesson5.java
@@ -3,15 +3,17 @@ public class JavaLesson5 {
     static double myPI = 3.14159; // class variable
 
     public static void main(String[] args) {
-        addThem(1, 2);
-        System.out.println("Global " + myPI);
+        System.out.println(addThem(1,2));
+//        System.out.println("Global " + myPI);
     }
     public static int addThem(int a, int b) {
-        double smallPI = 3.140; // local variable
-        System.out.println("Global " + myPI);
-
-        myPI = myPI + 3.0;
-        System.out.println("Local " + myPI);
-        return 1;
+//        double smallPI = 3.140; // local variable
+//        System.out.println("Global " + myPI);
+//
+//        myPI = myPI + 3.0;
+        int c = a + b;
+        return c;
+//        System.out.println("Local " + myPI);
+//        return 1;
     }
 }
