49a205b601524469c3472d76cb08ef3a34ff6479
Abraham Bueno <abueno@localhost.localdomain>
2019-01-03 23:05:03 -0600

    Accessed the methods and data from Monster from JavaLesson7. There was a monster, Frank, created with the constructor class Monster, inside of JavaLesson7 class.

---
+++ src/JavaLesson7.java
@@ -0,0 +1,8 @@
+public class JavaLesson7 {
+
+    public static void main(String[] args) {
+        Monster Frank = new Monster();
+        Frank.name = "Frank2";
+        System.out.println(Frank.name + " has an attack of " + Frank.getAttack());
+    }
+}
