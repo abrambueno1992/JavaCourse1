7523cd5184b68893d89d102922865d4223108bad
Abraham Bueno <abueno@localhost.localdomain>
2019-01-03 18:27:03 -0600

    Examples of simple for loops.

--- src/JavaLesson4.java
+++ src/JavaLesson4.java
@@ -51,5 +51,16 @@ public class JavaLesson4 {
             System.out.println(k);
             k++;
         } while (k < 10);
+
+
+        // for (declare interator; conditional statement; change iterator)
+        for (int l = 10; l >= 1; l--) {
+            System.out.println(l);
+        }
+
+        int m, n;
+        for (m = 1, n = 2; m <= 9; m += 2, n += 2) {
+            System.out.println(m + "\n" + n);
+        }
     }
 }
