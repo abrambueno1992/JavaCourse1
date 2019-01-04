82cec914e5fc26708b9797579018668975343e7d
Abraham Bueno <abueno@localhost.localdomain>
2019-01-03 20:41:16 -0600

    Simple error handling, runtime error.

--- src/JavaLesson6.java
+++ src/JavaLesson6.java
@@ -12,7 +12,15 @@
     IOException
  */
 
-
-
 public class JavaLesson6 {
+    public static void main(String[] args) {
+        divideByZero(2);
+    }
+    public static void divideByZero(int a) {
+        try {
+            System.out.println(a / 0);
+        } catch (ArithmeticException e) {
+            System.out.println("you can't do that");
+        }
+    }
 }
