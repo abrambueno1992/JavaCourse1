122f8899981179b3319d6a779bf4771d1441e55a
Abraham Bueno <abueno@localhost.localdomain>
2019-01-03 18:38:03 -0600

    Learned difference between local variables and global variables. Also learned about scope, and how you can actually change the values of global variables.

---
+++ src/JavaLesson5.java
@@ -0,0 +1,17 @@
+import java.util.Scanner;
+public class JavaLesson5 {
+    static double myPI = 3.14159; // class variable
+
+    public static void main(String[] args) {
+        addThem(1, 2);
+        System.out.println("Global " + myPI);
+    }
+    public static int addThem(int a, int b) {
+        double smallPI = 3.140; // local variable
+        System.out.println("Global " + myPI);
+
+        myPI = myPI + 3.0;
+        System.out.println("Local " + myPI);
+        return 1;
+    }
+}
