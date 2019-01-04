77afda6661a398f18b910aef5c5da1200279b4eb
Abraham Bueno <abueno@localhost.localdomain>
2019-01-03 20:56:25 -0600

    Many examples of different types of exceptions, and how to handle them with try and catch blocks. There is also a method to avoid errors from login, and a finaly block. Finally is always executed.

--- src/JavaLesson6.java
+++ src/JavaLesson6.java
@@ -11,10 +11,53 @@
     InputMismatchException
     IOException
  */
-
+import java.io.FileInputStream;
+import java.io.FileNotFoundException;
+import java.io.IOException;
+import java.util.*;
 public class JavaLesson6 {
+    static Scanner userInput = new Scanner(System.in);
+
     public static void main(String[] args) {
         divideByZero(2);
+        System.out.println("How old are you?");
+        int age = checkValidAge();
+        if (age != 0) {
+            System.out.println("You are " + age + " years old");
+        }
+
+        getAFile("./someStuff.txt");
+
+    }
+
+    public static void getAFile(String fileName) {
+        try {
+            FileInputStream file = new FileInputStream(fileName);
+
+        } catch (FileNotFoundException e) {
+            System.out.println("Sorry can't find that file");
+        }
+        catch(IOException e) {
+            System.out.println("Unknown IO Error");
+        }
+        catch (ClassNotFoundException e) {
+            
+        }
+        catch(Exception e) {
+            System.out.println("Some error occured.");
+        }
+        finally {
+            System.out.println("Always called");
+        }
+    }
+    public static int checkValidAge() {
+        try {
+            return userInput.nextInt();
+        } catch (InputMismatchException e) {
+            userInput.next();
+            System.out.println("That isn't a whole number");
+            return 0;
+        }
     }
     public static void divideByZero(int a) {
         try {
@@ -22,8 +65,9 @@ public class JavaLesson6 {
         } catch (ArithmeticException e) {
             System.out.println("you can't do that");
             System.out.println(e.getMessage());
-            System.out.println(e.toString());
-            e.printStackTrace();
+//            System.out.println(e.toString());
+//            e.printStackTrace();
         }
+
     }
 }
