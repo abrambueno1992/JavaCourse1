e50bd9b155740f25422f129bccad6a0b6a8b94f9
Abraham Bueno <abueno@localhost.localdomain>
2019-01-04 22:43:16 -0600

    Fixed the display of the multidimensional array. I was printing with a new lint, println, instead of using inline printing, print. Changing the print corrected the output, now it displays the right output.

--- src/JavaLesson9.java
+++ src/JavaLesson9.java
@@ -39,7 +39,7 @@ public class JavaLesson9 {
 
         for (int i = 0; i < multiArray.length; i++) {
             for (int j = 0; j < multiArray[i].length; j++) {
-                System.out.println("| " + multiArray[i][j] + " ");
+                System.out.print("| " + multiArray[i][j] + " ");
             }
             System.out.println("|");
         }
