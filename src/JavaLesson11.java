a40067f5f0327febb41f2d00b737bfd758ca5d51
Abraham Bueno <abueno@localhost.localdomain>
2019-01-05 20:42:15 -0600

    Simple examples using Array lists.

--- src/JavaLesson11.java
+++ src/JavaLesson11.java
@@ -1,5 +1,29 @@
 import org.apache.commons.lang3.ArrayUtils;
+
+import java.util.ArrayList;
 import java.util.Iterator;
 import java.util.Arrays;
 public class JavaLesson11 {
+    public static void main(String[] args) {
+        ArrayList arrayListOne;
+
+        arrayListOne = new ArrayList();
+
+        ArrayList arrayListTwo = new ArrayList();
+
+        ArrayList<String> names = new ArrayList<String>();
+
+        names.add("John Smith");
+        names.add("Moe Smith");
+        names.add("Oliver More");
+        names.add("Johnny Casshh");
+
+        names.add(2, "Jack Ryan");
+
+        
+        names.set(0, "John Adams");
+        for (int i = 0; i < names.size(); i++) {
+            System.out.println(names.get(i));
+        }
+    }
 }
