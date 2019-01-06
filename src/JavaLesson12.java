d7ad882e409d33abc9418e4720b1368cb65221c7
Abraham Bueno <abueno@localhost.localdomain>
2019-01-05 21:26:17 -0600

    Created simple LinkedList, with the add method and printed the result with an enhanced for loop.

--- src/JavaLesson12.java
+++ src/JavaLesson12.java
@@ -2,6 +2,15 @@ import java.util.Arrays;
 import java.util.LinkedList;
 public class JavaLesson12 {
     public static void main(String[] args) {
-        
+        LinkedList linkedListOne = new LinkedList();
+
+        LinkedList<String> names = new LinkedList<String>(); // restricted to String
+
+        names.add("Ahmed Banana");
+        names.add("Ali Said");
+
+        for (String name : names) {
+            System.out.println(name);
+        }
     }
 }
