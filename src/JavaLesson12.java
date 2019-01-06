3005c022c08e41e1f70c0e40704ad8a625636ee4
Abraham Bueno <abueno@localhost.localdomain>
2019-01-05 21:31:04 -0600

    Added functionality  set to replace value with a new one, and the remove functionality to delete values. Remove takes in either an integer for the index or the value of the element to be removed (a string).

--- src/JavaLesson12.java
+++ src/JavaLesson12.java
@@ -12,6 +12,13 @@ public class JavaLesson12 {
         names.addLast("Nathy Martian"); //last
         names.addFirst("Josh Smash"); // first
 
+        names.add(0, "Noah Pete"); // 0 index
+
+        names.set(2, "Paul Newname"); // replace value at index 2
+
+        names.remove(4); // removes element at index 4
+        names.remove("Noah Pete"); // removes name
+
         for (String name : names) {
             System.out.println(name);
         }
