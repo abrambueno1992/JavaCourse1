9b9e518a5b073d3c97209f1cd898708935f2d90c
Abraham Bueno <abueno@localhost.localdomain>
2019-01-06 23:50:56 -0600

    JavaLesson14 class has been created and created a new Vehicle object, and called upon getSpeed() and getWheels().

---
+++ src/JavaLesson15.java
@@ -0,0 +1,8 @@
+public class JavaLesson15 {
+    public static void main(String[] args) {
+        Vehicle car = new Vehicle(4, 100.00);
+        System.out.println("Car Max Speed " + car.getSpeed());
+        System.out.println("Num of Wheels " + car.getWheel());
+
+    }
+}
