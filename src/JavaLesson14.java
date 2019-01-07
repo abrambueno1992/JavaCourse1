ca8f00e348ec1910543a953ca86571324e74000d
Abraham Bueno <abueno@localhost.localdomain>
2019-01-06 22:09:33 -0600

    Added variable Animals with initialization as Cats. Java was able to confirm that it's actually a Cats varialble and not an Animals varialble. Passing the Animals object, which is actually a Cats object, the .walkAround() method executed from the Cats class and not the Animal class.

--- src/JavaLesson14.java
+++ src/JavaLesson14.java
@@ -10,5 +10,18 @@ public class JavaLesson14 {
         System.out.println(morris.getName());
         System.out.println(morris.favFood);
         System.out.println(morris.favToy);
+
+        Animals tabby = new Cats("Tammy", "Solamander", "Bass");
+
+        acceptAnimal(tabby);
+    }
+
+    public static void acceptAnimal(Animals randAnimal) {
+        System.out.println();
+        System.out.println(randAnimal.getName());
+        System.out.println(randAnimal.favFood);
+        System.out.println();
+
+        randAnimal.walkAround();
     }
 }
