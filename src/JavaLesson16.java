347199df75722d3c796a9fe501c2553967cc76f5
Abraham Bueno <abueno@localhost.localdomain>
2019-01-08 21:57:27 -0600

    Compared Object initialized as a vehicle and Vehicle initialized as a vehicle. They're not equal

--- src/JavaLesson16.java
+++ src/JavaLesson16.java
@@ -2,5 +2,8 @@ public class JavaLesson16 {
     public static void main(String[] args) {
         Object superCar = new Vehicle();
         System.out.println(((Vehicle) superCar).getSpeed());
+        Vehicle superTruck = new Vehicle();
+
+        System.out.println(superCar.equals(superTruck));
     }
 }
