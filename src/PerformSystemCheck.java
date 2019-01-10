aa3ed253f6e971a481450182062f2cccd8e76823
Abraham Bueno <abueno@localhost.localdomain>
2019-01-09 23:21:11 -0600

    Created ReentrantLock object, and used a method to lock and unlock the method with lock.lock() and lock.unlock()... synchronized could also be used to lock a method.

--- src/PerformSystemCheck.java
+++ src/PerformSystemCheck.java
@@ -3,16 +3,19 @@ import java.util.concurrent.locks.ReentrantLock;
 public class PerformSystemCheck implements Runnable {
     private String checkWhat;
 
-
-    public PerformSystemCheck(checkWhat) {
+    ReentrantLock lock = new ReentrantLock();
+    public PerformSystemCheck(String checkWhat) {
         this.checkWhat = checkWhat;
     }
 
     public PerformSystemCheck() {
 
     }
-
+// synchronized  allows method to be locked
     public void run() {
+        lock.lock(); // another way to lock this method
+        System.out.println("Checking " + this.checkWhat);
+        lock.unlock(); // unlocks the method
 
     }
 }
