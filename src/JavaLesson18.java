2ba32d13d9c711720e498af1346b09b3cfae9326
Abraham Bueno <abueno@localhost.localdomain>
2019-01-09 23:38:02 -0600

    Imported two new functionalities java.util.concurrent.ScheduledThreadPoolExecutor and java.util.concurrent.TimeUnit.*

--- src/JavaLesson18.java
+++ src/JavaLesson18.java
@@ -1,5 +1,7 @@
+import java.util.concurrent.ScheduledThreadPoolExecutor;
+import java.util.concurrent.TimeUnit.*;
 public class JavaLesson18 {
     public static void main(String[] args) {
-        
+
     }
 }
