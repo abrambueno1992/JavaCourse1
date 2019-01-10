e090306fe46fbff0d4836afbf2c75829fba5c1e7
Abraham Bueno <abueno@localhost.localdomain>
2019-01-09 23:17:11 -0600

    Created Constructor PerformSystemCheck(), takes in checkWhat.

--- src/PerformSystemCheck.java
+++ src/PerformSystemCheck.java
@@ -1,7 +1,13 @@
 import java.util.concurrent.locks.ReentrantLock;
 
 public class PerformSystemCheck implements Runnable {
-    
+    private String checkWhat;
+
+
+    public PerformSystemCheck(checkWhat) {
+        this.checkWhat = checkWhat;
+    }
+
     public PerformSystemCheck() {
 
     }
