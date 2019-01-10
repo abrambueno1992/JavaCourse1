8d5dace5c9c9a4f2a35cab9e0698d74d1cca5e92
Abraham Bueno <abueno@localhost.localdomain>
2019-01-09 23:45:24 -0600

    Created ScheduledThreadPoolExecutor object and used scheduleAtFixedRate() to call CheckSysteTime() or PerformSystemCheck() with the appropriate values for each method, they took in initial delay, period and time unit.

--- src/JavaLesson18.java
+++ src/JavaLesson18.java
@@ -1,7 +1,19 @@
 import java.util.concurrent.ScheduledThreadPoolExecutor;
+import java.util.concurrent.TimeUnit;
 import java.util.concurrent.TimeUnit.*;
 public class JavaLesson18 {
     public static void main(String[] args) {
+        addThreadsToPool();
+    }
+
+    public static void addThreadsToPool() {
+        ScheduledThreadPoolExecutor eventPool = new ScheduledThreadPoolExecutor(5);
+
+        eventPool.scheduleAtFixedRate(new CheckSystemTime(), 0, 2, TimeUnit.SECONDS);
+        eventPool.scheduleAtFixedRate(new PerformSystemCheck("Mail"), 5, 5, TimeUnit.SECONDS);
+        eventPool.scheduleAtFixedRate(new PerformSystemCheck("Calendar"), 10, 10, TimeUnit.SECONDS);
+
+        System.out.println("Number of Threads: " + Thread.activeCount() );
 
     }
 }
