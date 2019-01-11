4a8f4db024beb70b72d614acde5b83a9ca63e747
Abraham Bueno <abueno@localhost.localdomain>
2019-01-10 22:20:10 -0600

    Initialized JavaLesson20 and imported three things, Dimension and Toolkit from java.awt, and javax.swing.*

---
+++ src/JavaLesson20.java
@@ -0,0 +1,12 @@
+import java.awt.Dimension;
+import java.awt.Toolkit;
+import javax.swing.*;
+public class JavaLesson20 extends JFrame {
+    public static void main(String[] args) {
+
+    }
+
+    public JavaLesson20() {
+
+    }
+}
