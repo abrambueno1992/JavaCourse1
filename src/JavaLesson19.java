84bcb17ea657bcbef075097e185b798f4045b983
Abraham Bueno <abueno@localhost.localdomain>
2019-01-10 21:12:24 -0600

    Comments with some simple ways of using regex to find characters.

--- src/JavaLesson19.java
+++ src/JavaLesson19.java
@@ -2,5 +2,16 @@ import java.util.regex.*;
 public class JavaLesson19 {
     public static void main(String[] args) {
 
+        String longString = " Aaab Band CA 1357 AA (555)555-555 checking";
+        String strangeString = " 1Z bbb *** *** {{{ {{ { ";
+
+        /*
+        [A-Z], [0-9]
+        \\s white space
+        \\S no white space
+        {n, m}
+         */
+        // Word that is 2 to 20 characters in length
+        //[A-Za-z]{2,20} or \\w{2,20}
     }
 }
