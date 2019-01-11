b5f0d40d4102c979213fe517392f25601c2c70f5
Abraham Bueno <abueno@localhost.localdomain>
2019-01-10 22:36:37 -0600

    Used Toolkit and Dimension from library to set the exact positioning of the x and y coordinates for the window. Used Toolkit with Dimension, the Toolkit object gets the .getDefaultToolkit() and the Dimension object gets .getScreenSize(). The dimension object is what sets the x and y with .width or .height

--- src/JavaLesson20.java
+++ src/JavaLesson20.java
@@ -8,6 +8,16 @@ public class JavaLesson20 extends JFrame {
 
     public JavaLesson20() {
         this.setSize(400, 400);
+        // set position of window centered
+//        this.setLocationRelativeTo(null);
+        Toolkit tk = Toolkit.getDefaultToolkit();
+        Dimension dim = tk.getScreenSize();
+
+        int xPos = (dim.width / 2) - (this.getWidth() / 2);
+        int yPos = (dim.height / 2) - (this.getHeight() / 2);
+
+        this.setLocation(xPos, yPos);
+
         this.setVisible(true);
     }
 }
