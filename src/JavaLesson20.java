f9dbb3b6ca6c4fc13bcec312234f02af8a3745b7
Abraham Bueno <abueno@localhost.localdomain>
2019-01-10 22:23:46 -0600

    Called the constructor and used setSize() and setVisible() to true. A window poped open, with no content.

--- src/JavaLesson20.java
+++ src/JavaLesson20.java
@@ -3,10 +3,11 @@ import java.awt.Toolkit;
 import javax.swing.*;
 public class JavaLesson20 extends JFrame {
     public static void main(String[] args) {
-
+        new JavaLesson20();
     }
 
     public JavaLesson20() {
-
+        this.setSize(400, 400);
+        this.setVisible(true);
     }
 }
