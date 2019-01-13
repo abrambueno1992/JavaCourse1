a37c2156f8c2598a015bc3b506d0e8ce17866cc2
Abraham Bueno <abueno@localhost.localdomain>
2019-01-12 23:33:01 -0600

    Added Toolkit object, Dimension object, to the JavaLesson21() constructor

--- src/JavaLesson21.java
+++ src/JavaLesson21.java
@@ -8,13 +8,22 @@ public class JavaLesson21 extends JFrame {
     JTextField textField1;
     JTextArea textArea1;
     int buttonClicked;
-    
+
     public static void  main(String[] args) {
         new JavaLesson21();
     }
 
     public JavaLesson21() {
         this.setSize(400, 400);
+
+        Toolkit tk = Toolkit.getDefaultToolkit();
+
+        Dimension dim = tk.getScreenSize();
+
+        int xPos = (dim.width / 2) - (this.getWidth() / 2);
+        int yPos = (dim.height / 2) - (this.getHeight() / 2);
+
+        this.setLocation(xPos, yPos);
     }
 
 }
