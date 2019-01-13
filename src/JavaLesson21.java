8bd701308b79ccb79992cd4f75160e50e081ac64
Abraham Bueno <abueno@localhost.localdomain>
2019-01-12 23:27:38 -0600

    Import events library and creates JavaLesson21() constructor

--- src/JavaLesson21.java
+++ src/JavaLesson21.java
@@ -1,9 +1,15 @@
 import java.awt.Dimension;
 import java.awt.Toolkit;
 import javax.swing.*;
+
+import java.awt.event.*;
 public class JavaLesson21 extends JFrame {
     public static void  main(String[] args) {
         new JavaLesson21();
+    }
+
+    public JavaLesson21() {
+        this.setSize(400, 400);
     }
 
 }
